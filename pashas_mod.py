0ce29e47e5dde845574472d6de719f65b254def7
Usapaul <arandor-lok@yandex.ru>
2018-11-03 21:57:21 +0300

    add Planck_fun

---
+++ pashas_mod.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy
+from astropy import units as u
+from astropy.modeling.blackbody import blackbody_lambda
+
+
+# blackbody_lambda(wavelength, temperature)
+
