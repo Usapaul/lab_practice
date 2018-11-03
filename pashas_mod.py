5d22e4df6b377bcad74faf6a8f3074749adfeab1
Usapaul <arandor-lok@yandex.ru>
2018-11-03 22:03:12 +0300

    add Planck_fun

--- pashas_mod.py
+++ pashas_mod.py
@@ -9,4 +9,5 @@ from astropy.modeling.blackbody import blackbody_lambda
 # blackbody_lambda(wavelength, temperature)
 
 def Planck_fun(wavelength, temperature):
-    return blackbody_lambda(wavelength, temperature)
+    return blackbody_lambda(wavelength, temperature).value
+
